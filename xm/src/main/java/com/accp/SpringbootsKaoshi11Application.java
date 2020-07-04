85dcc396aeb5cde8ff02f3cdb2f1d4bb1734cf76
Administrator <Administrator@192.168.163.1>
2020-07-04 15:21:50 +0800

    初始化

---
+++ xm/src/main/java/com/accp/SpringbootsKaoshi11Application.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsKaoshi11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsKaoshi11Application.class, args);
+	}
+
+}
---
+++ xm/src/test/java/com/accp/SpringbootsKaoshi11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsKaoshi11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
